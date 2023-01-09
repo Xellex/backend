921b4c0a940be71b3701ed183f6541b54d88e367
Jorn Bouwmeester <jornbouwmeester@jorns-mbp.home>
2023-01-09 14:46:43 +0100

    nog een keer

---
+++ src/main/java/com/example/backend/model/Email.java
@@ -0,0 +1,48 @@
+package com.example.backend.model;
+
+public class Email {
+
+	private String name;
+	private String to;
+	private String subject;
+	private String message;
+
+	public Email(String name, String to, String subject, String message) {
+		this.name = name;
+		this.to = to;
+		this.subject = subject;
+		this.message = message;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}

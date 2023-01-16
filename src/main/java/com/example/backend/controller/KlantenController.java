7b2525650a44a954155b29652ae51f9cefb7b7c4
JobRutgerJongebloet <j.jongebloet@outlook.com>
2023-01-16 12:07:47 +0100

    postmapping ipv getmapping bij inloggen

--- src/main/java/com/example/backend/controller/KlantenController.java
+++ src/main/java/com/example/backend/controller/KlantenController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -80,7 +81,7 @@ public class KlantenController {
 		return null;
 	}
 
-	@GetMapping("klanten/inloggen")
+	@PostMapping("klanten/inloggen")
 	public LoginResponseDTO checkEmailAndPassword(@RequestBody KlantDTO klant) {
 		ArrayList<String> errors = new ArrayList<String>();
 		

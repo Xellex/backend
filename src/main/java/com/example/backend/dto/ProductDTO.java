0490e809cb9c9e115ed8709737ec044524127120
JobRutgerJongebloet <j.jongebloet@outlook.com>
2023-01-23 15:11:24 +0100

    productDTO

---
+++ src/main/java/com/example/backend/dto/ProductDTO.java
@@ -0,0 +1,79 @@
+package com.example.backend.dto;
+import com.example.backend.model.Feestdagen;
+import com.example.backend.model.ProductCategorie;
+
+public class ProductDTO {
+
+	private long id;
+	private String naam;
+	private String beschrijving;
+	private ProductCategorie categorie;
+	private Feestdagen feestdag;
+	private int voorraad;
+	private double kosten;
+	private double inkoop;
+
+	public double getInkoop() {
+		return inkoop;
+	}
+
+	public void setInkoop(double inkoop) {
+		this.inkoop = inkoop;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Feestdagen getFeestdag() {
+		return feestdag;
+	}
+
+	public void setFeestdag(Feestdagen feestdag) {
+		this.feestdag = feestdag;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public String getBeschrijving() {
+		return beschrijving;
+	}
+
+	public void setBeschrijving(String beschrijving) {
+		this.beschrijving = beschrijving;
+	}
+
+	public ProductCategorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(ProductCategorie categorie) {
+		this.categorie = categorie;
+	}
+
+	public int getVoorraad() {
+		return voorraad;
+	}
+
+	public void setVoorraad(int voorraad) {
+		this.voorraad = voorraad;
+	}
+
+	public double getKosten() {
+		return kosten;
+	}
+
+	public void setKosten(double kosten) {
+		this.kosten = kosten;
+	}
+}

a340d73c064d3928575880782afae9a87185afff
Aman Ahmed <aman16.aa17@gmail.com>
2023-01-24 10:50:01 +0100

    Sinterklaar aangepast naar Sinterklaas

--- src/main/java/com/example/backend/model/Feestdagen.java
+++ src/main/java/com/example/backend/model/Feestdagen.java
@@ -1,7 +1,7 @@
 package com.example.backend.model;
 
 public enum Feestdagen {
-	
-	Halloween, Kerstmis, Koningsdag, Moederdag, OudEnNieuw, Pasen, Sinterklaar, SintMaarten, Suikerfeest, Valentijnsdag
-	
+
+	Halloween, Kerstmis, Koningsdag, Moederdag, OudEnNieuw, Pasen, Sinterklaas, SintMaarten, Suikerfeest, Valentijnsdag
+
 }
